7d07aab9847403cf4e0cc3ede8d7c88c6f870157
Ismail Mustafa <ismailmustafa@rocketmail.com>
2018-01-29 23:39:36 -0500

    Clean up example

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ module Main where
 import Network.Handwriting
 import System.Environment   (getEnv)
 import Data.ByteString.Lazy (writeFile, ByteString)
-import Prelude hiding       (writeFile) 
+import Prelude hiding       (writeFile)
 import System.Directory     (getCurrentDirectory)
 import System.FilePath      ((</>))
 import System.Random        (newStdGen, randomR)
@@ -13,34 +13,31 @@ import Data.Text            (unpack)
 main :: IO ()
 main = do
   -- Credentials
-  key <- getEnv "KEY"
-  secret <- getEnv "SECRET"
-  let creds = Credentials key secret
+  creds        <- Credentials <$> getEnv "KEY" <*> getEnv "SECRET"
 
   -- Get all handwritings
   handwritings <- getHandwritings creds
 
   -- Generate random number in range of handwritings
-  g <- newStdGen
+  g            <- newStdGen
   let rand = randomR (0, length handwritings - 1) g
 
   -- Select random handwriting font
-  let id = unpack $ handwritingId $ handwritings !! fst rand
+  let id   = unpack $ handwritingId $ handwritings !! fst rand
   handwriting <- getHandwriting creds id
   print handwriting
 
   -- Generate image
-  let params = defaultImageParams {
-    format = PNG,
-    hId = Just "31SB2CWG00DZ",
-    size = Just 205,
-    lineSpacing = Just 2,
-    lineSpacingVariance = Just 0.2,
-    wordSpacingVariance = Just 0.4,
-    width = Just 800,
-    color = Just (0,0,0),
-    randomSeed = Randomize
-    }
+  let params = defaultImageParams { format              = PNG
+                                  , hId                 = Just "31SB2CWG00DZ"
+                                  , size                = Just 205
+                                  , lineSpacing         = Just 2
+                                  , lineSpacingVariance = Just 0.2
+                                  , wordSpacingVariance = Just 0.4
+                                  , width               = Just 800
+                                  , color               = Just (0, 0, 0)
+                                  , randomSeed          = Randomize
+                                  }
   imageByteString <- renderImage creds params "Hello World!"
 
   -- Write image to current directory
@@ -51,5 +48,4 @@ writeHandwritingFile image name = do
   dir <- getCurrentDirectory
   let imageDir = dir </> name
   writeFile imageDir image
-
 
